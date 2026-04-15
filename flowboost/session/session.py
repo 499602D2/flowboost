d75e14f4c49a86114869cd12749ca757c9cf5f97
499602D2 <46869890+499602D2@users.noreply.github.com>
2026-04-15 22:56:48 +0300

    Bump deps + format

--- flowboost/session/session.py
+++ flowboost/session/session.py
@@ -2,7 +2,6 @@ import importlib.resources as pkg_resources
 import json
 import logging
 import shutil
-import sys
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Any, Optional, Union, Literal
@@ -13,7 +12,10 @@ from flowboost.openfoam.case import Case, path_is_foam_dir, unique_id
 from flowboost.openfoam.dictionary import Dictionary, DictionaryLink, DictionaryReader
 from flowboost.openfoam.types import FOAMType
 from flowboost.optimizer.acquisition_offload import OFFLOAD_SCRIPT
-from flowboost.optimizer.backend import DEFAULT_OFFLOAD_RESULT_FNAME, OptimizationComplete
+from flowboost.optimizer.backend import (
+    DEFAULT_OFFLOAD_RESULT_FNAME,
+    OptimizationComplete,
+)
 from flowboost.optimizer.interfaces.Ax import Backend
 from flowboost.optimizer.search_space import Dimension
 
